d22bd2171f7f3ba753146e69a8505438fb79fb72
darin-m-myers <107961905+darin-m-myers@users.noreply.github.com>
2022-07-13 22:04:37 -0500

    Add County Code

--- PyPoll.py
+++ PyPoll.py
@@ -9,6 +9,8 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#### Begin Gathering Candiate Summary
+
 # Initialize a total vote counter.
 total_votes = 0
 
@@ -21,6 +23,18 @@ winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
 
+# Initialize a county total vote counter.
+total_county_votes = 0
+
+# County Options and Votes
+county_list = []
+county_votes = {}
+
+# Winning County and Winning Count Tracker
+winning_county = ""
+winning_county_count = 0
+winning_county_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -32,6 +46,7 @@ with open(file_to_load) as election_data:
     # Print each row in the CSV file.
     for row in file_reader:
         
+        ### CANDIDATE DATA
         # Count the total number of votes cast
         total_votes += 1
 
@@ -48,22 +63,78 @@ with open(file_to_load) as election_data:
 
         # Record the total votes for each candidate
         candidate_votes[candidate_name] += 1
+        
+        ### COUNTY DATA
+        # Record the names for all the counties
+        county_name = row[1]
+
+        if county_name not in county_list:
+
+            # Add it to the list of county.
+            county_list.append(county_name)
+
+            # Begin tracking that county's vote count.
+            county_votes[county_name] = 0
+
+        # Record the total votes for each county
+        county_votes[county_name] += 1
 
 # Save the results to our text file.
 with open(file_to_save, "w") as txt_file:
 
-    # Print the final vote count to the terminal.
+    # Set consisitent dashed line breaks
     horizontal_line = "-" * 25 + "\n"
+
+    ### Election Summary Data
+    # Print the final vote count to the terminal.
     election_results = (
         f"\nElection Results\n"
         f"{horizontal_line}"
         f"Total Votes: {total_votes:,}\n"
-        f"{horizontal_line}")
+        f"{horizontal_line}"
+        f"\nCountyVotes:\n")
     print(election_results, end="")
 
     # Save the final vote count to the text file.
     txt_file.write(election_results)
 
+    # Record the percentage of votes each county
+    for county_name in county_votes:
+
+        votes = county_votes[county_name]
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+        # Provide a results of the election
+        county_election_results = (f"{county_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        # Print each county, their voter count, and percentage to the terminal.
+        print(county_election_results)
+
+        #  Save the county results to our text file.
+        txt_file.write(county_election_results)
+
+        if (votes > winning_county_count) and (vote_percentage > winning_county_percentage):
+            # If true then set winning_count = votes and winning_percent =
+            # vote_percentage.
+            winning_county_count = votes
+            winning_county_percentage = vote_percentage
+            # And, set the winning_county equal to the county's name.
+            winning_county = county_name   
+
+    winning_county_summary = (
+        f"\n{horizontal_line}"  
+        f"Largest County Turnout: {winning_county}\n"
+        f"{horizontal_line}")
+    print(winning_county_summary)
+
+    txt_file.write(winning_county_summary)
+    
+
+    ### CANDIDATE DATA
+    # Initialize variables
+    votes = 0
+    vote_percentage = 0.0
+    
     # Record the percentage of votes each candidate won
     for candidate_name in candidate_votes:
 
@@ -80,7 +151,7 @@ with open(file_to_save, "w") as txt_file:
         txt_file.write(candidate_results)
 
         if (votes > winning_count) and (vote_percentage > winning_percentage):
-            # If true then set winning_count = votes and winning_percent =
+            # If true then set winning_count = votes and winning_percentage =
             # vote_percentage.
             winning_count = votes
             winning_percentage = vote_percentage
